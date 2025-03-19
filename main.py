7b3d8543002fcdbaaf11314e27bfad307ff3b98e
nvbvjason <jasongadewoltz@gmail.com>
2025-03-19 17:03:48 +0100

    fourth

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, 'r') as file:
+        return file.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    num_words = len(text.split())
+    print(f"{num_words} words found in the document")
+
+if __name__ == "__main__":
+    main()
